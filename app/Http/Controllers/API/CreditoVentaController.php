b9f31e16c882316724e82a89014a588e78ed7097
CristianLedezmaS <ledezmac232@gmail.com>
2025-12-06 11:02:08 -0400

    feat: Add CuotaCredito model import to CreditoVentaController for enhanced functionality

--- app/Http/Controllers/API/CreditoVentaController.php
+++ app/Http/Controllers/API/CreditoVentaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\CreditoVenta;
+use App\Models\CuotaCredito;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
